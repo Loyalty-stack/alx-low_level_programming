82a8973fd3c28d0235b1c26c36fcd3b28bd55a83
Loyalty-stack <Bamitalemajek01@gmail.com>
2022-09-11 13:24:31 -0700

    reversed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints reverse lowercase alphabets
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--) 
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
