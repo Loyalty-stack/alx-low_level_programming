8e34ea05f18ca67a09c181a37411907058386f4f
Loyalty-stack <Bamitalemajek01@gmail.com>
2022-09-14 05:41:26 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints out _putchar with a new line.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	_putchar("_putchar);
+
+	_putchar('\n');
+	return (0);
+}
