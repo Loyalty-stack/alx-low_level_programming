df12dfa3560e6e6f35cfdd665daaafb475f1689f
Loyalty-stack <Bamitalemajek01@gmail.com>
2022-09-14 11:42:44 -0700

    loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- * _islower - shows 1 if the input is a lowercas * e character, Another cases shows 0.
+ * _islower - shows 1 if the input is a lowercas *e character, Another cases shows 0.
  *
- * Return: Always 0 (success)
+ * @c: the character in ASCII code
+ *
+ * Return: 1 for lowercase character, 0 for the other 
  */
 int _islower(int c)
 {
-	if (c >=97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
