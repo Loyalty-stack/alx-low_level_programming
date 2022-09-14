a0217e4df4e6d876b87e95a2661eb6fb6689547f
Loyalty-stack <Bamitalemajek01@gmail.com>
2022-09-14 09:33:03 -0700

    nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-#include "main.h" 
+#include "main.h"
 /**
- * main - prints out _putchar with a new line.
+ * main - prints _putchar as a message
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	_putchar("_putchar");
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
 
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
 	_putchar('\n');
 	return (0);
 }
