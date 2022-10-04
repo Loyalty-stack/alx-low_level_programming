5ade6e9eb412644f0c75c3120d066aefcc1192de
Loyalty-stack <Bamitalemajek01@gmail.com>
2022-10-04 05:47:51 -0700

    task 0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - shows 1 if lowercase character, Another cases shows 0.
+ *
+ * @c: the character in ASCII code
+ *
+ * Return: 1 for lowercase character, 0 for the other
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
